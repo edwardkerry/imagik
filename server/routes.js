cc18e27f92ee9bb885bc286269b9be2aa1bef709
edwardkerry <edwardkerry@gmail.com>
2016-01-17 23:17:25 +0000

    WIP Scoring almost complete, need to add final bowl

--- server/routes.js
+++ server/routes.js
@@ -9,5 +9,6 @@ var express = require('express'),
     router.post('/images', image.create);
     router.post('/images/:image_id/like', image.like);
     router.post('/images/:image_id/comment', image.comment);
+    router.delete('/images/:image_id', image.remove);
     app.use(router);
   };
